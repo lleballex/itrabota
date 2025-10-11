4d2905615778636d87c43ecfb736fd5480af6509
lleballex <lleballex@gmail.com>
2025-10-11 16:00:05 +0300

    feat: add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core"
 
 import { AppModule } from "./app.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const swaggerConfig = new DocumentBuilder().setTitle("айтиработа.рф").build()
+
+  SwaggerModule.setup("api", app, () =>
+    SwaggerModule.createDocument(app, swaggerConfig),
+  )
+
   await app.listen(process.env.PORT ?? 3000)
 }
 
