93f479bc839a5d7fc1e6efc6ae77271ae8bd5a76
lleballex <lleballex@gmail.com>
2025-11-03 15:41:54 +0300

    feat(front): remove mocked data from useMe

--- frontend/src/api/auth/get-me.ts
+++ frontend/src/api/auth/get-me.ts
@@ -1,36 +1,10 @@
 import { createUseQuery } from "@/api/lib/create-use-query"
 import { axios } from "@/api/lib/axios"
 
-import { User, UserRole } from "@/types/entities/user"
+import { User } from "@/types/entities/user"
 
-const mockedData: User = {
-  id: "ad",
-  createdAt: "sf",
-  updatedAt: "af",
-  email: "lleballex@gmail.com",
-  role: UserRole.Recruiter,
-  passwordChangedAt: "2022-01-01T10:00Z",
-  profile: {
-    id: "aa",
-    createdAt: "sf",
-    updatedAt: "af",
-    firstName: "Алексей",
-    lastName: "Лебедев",
-    patronymic: "Юрьевич",
-    company: {
-      id: "ad",
-      createdAt: "sf",
-      updatedAt: "af",
-      name: "Технопарк",
-      logo: null,
-      url: null,
-    },
-  },
-}
-
-export const useMe = createUseQuery("me", () =>
-  axios
-    .get<User>("/me")
-    .then((res) => res.data)
-    .catch(() => mockedData)
+export const useMe = createUseQuery(
+  "me",
+  () => axios.get<User>("/me").then((res) => res.data),
+  { retry: false }
 )
