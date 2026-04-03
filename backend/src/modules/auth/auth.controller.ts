80b51f2b3be8b6d50659583f2b60ffd842346ba3
lleballex <lleballex@gmail.com>
2026-04-04 00:24:23 +0300

    fix(auth): protect logout and enforce strict secure accessToken cookie

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -9,10 +9,9 @@ import {
 } from "@nestjs/common"
 import { Response } from "express"
 
-import { UsersService } from "@/modules/users/users.service"
-
 import { LoginDto } from "./dto/login.dto"
 import { CurrentUser } from "./decorators/current-user.decorator"
+import { Auth } from "./decorators/auth.decorator"
 import { ICurrentUser } from "./interfaces/current-user.interface"
 import { LocalAuthGuard } from "./guards/local-auth.guard"
 import { AuthService } from "./auth.service"
@@ -20,15 +19,18 @@ import { RegisterDto } from "./dto/register.dto"
 
 @Controller("auth")
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly usersService: UsersService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   private setAccessToken(token: string, res: Response) {
     res.cookie("accessToken", token, {
-      httpOnly: true, // TODO: secure cookie, change options
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
     })
+  }
+
+  private removeAccessToken(res: Response) {
+    res.clearCookie("accessToken")
   }
 
   @Post("login")
@@ -52,10 +54,10 @@ export class AuthController {
     this.setAccessToken(token, res)
   }
 
-  // TODO: should I add auth?
   @Post("logout")
+  @Auth()
   @HttpCode(HttpStatus.NO_CONTENT)
   logout(@Res({ passthrough: true }) res: Response) {
-    res.clearCookie("accessToken")
+    this.removeAccessToken(res)
   }
 }
